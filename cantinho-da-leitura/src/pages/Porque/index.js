5d785d98766bc04d600ef14e967115df4adf4705
cecisousa <ceciliasousa21@gmail.com>
2019-12-09 22:49:54 -0200

    inicia pagina por que ler

---
+++ cantinho-da-leitura/src/pages/Porque/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../components/Header'
+import './styles.css'
+
+const Porque = () => {
+    return (
+        <section>
+            <Header
+                classe="porque-header"
+                titulo="Por que ler nos dias de hoje?"
+            />
+        </section>
+    )
+}
+
+export default Porque;
