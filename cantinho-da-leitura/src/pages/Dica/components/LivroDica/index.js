33d17a45cb25c620fe9abe72ffa6652f9172750b
cecisousa <ceciliasousa21@gmail.com>
2019-12-12 15:44:06 -0200

    inicia responsividade da tela de dica

--- cantinho-da-leitura/src/pages/Dica/components/LivroDica/index.js
+++ cantinho-da-leitura/src/pages/Dica/components/LivroDica/index.js
@@ -4,10 +4,12 @@ import './styles.css'
 function LivroDica(props) {
   return (
     <div className="divContentDica">
-      <img className="imagemLivroDica" src={props.imagem} alt="Crédito da imagem: site da Amazon" />
       <div className="divContentText">
         <h3 className="tituloDica">{props.titulo}</h3>
         <p className="autorDica">{props.autor}</p>
+      </div>
+      <div className="divImagemRandom">
+        <img className="imagemLivroDica" src={props.imagem} alt="Crédito da imagem: site da Amazon" />
       </div>
     </div>
   )
