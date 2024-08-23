d8bc2e9703b0ec23a8021f002601cdeee06de15a
Cecilia Sousa <cecilia.sousa@sumup.com>
2024-08-23 21:07:33 +0200

    Format using Prettier

--- cantinho-da-leitura/src/pages/Melhores/index.js
+++ cantinho-da-leitura/src/pages/Melhores/index.js
@@ -1,64 +1,73 @@
-import React, { Fragment } from 'react';
-import Header from '../../components/Header';
-import LivroMelhores from './components/LivroMelhores';
-import Intro from '../../components/Intro'
-import { getMelhores } from '../../service/nytimes';
-import NYT from '../../assets/img/nyt.jpg';
-import './styles.css'
+import React, { Fragment } from "react";
+import Header from "../../components/Header";
+import LivroMelhores from "./components/LivroMelhores";
+import Intro from "../../components/Intro";
+import { getMelhores } from "../../service/nytimes";
+import NYT from "../../assets/img/nyt.jpg";
+import "./styles.css";
 
 class Melhores extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            livros: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      livros: [],
+    };
+  }
 
-    componentDidMount() {
-        getMelhores()
-            .then(response => {
-                this.setState({
-                    livros: response.data.results.lists,
-                })
-            })
+  componentDidMount() {
+    getMelhores()
+      .then((response) => {
+        this.setState({
+          livros: response.data.results.lists,
+        });
+      })
 
-            .catch(error => {
-                console.log(error);
-            })
-    }
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    render() {
-        return (
-            <Fragment>
-                <Header
-                    titulo="Os melhores livros da semana"
-                    classe="melhores-header"
-                />
-                <section className="container melhores">
-                    <Intro
-                        imagem={NYT}
-                        alt="Capa do jornal impresso The New York Times"
-                        paragrafo1={["O jornal ", <span key="indicadorMelhores" className='nomeSite'>The New York Times</span>, " é uma publicação norte-americana que circula diariamente desde 1851. Considerada uma referência no mundo do jornalismo, a publicação traz semanalmente uma lista dos livros que mais foram vendidos nos Estados Unidos."]}
-                        paragrafo2='Esses "best-sellers" são originalmente agrupados em diversas categorias e, aqui, você tem acesso à lista completa: confira quais foram os 90 livros mais populares na última semana!'
-                    />
-                    <div>
-                        {this.state.livros.length > 0
-                            ? this.state.livros.map(item => {
-                                return item.flatMap(livro => {
-                                    return <div key={livro.primary_isbn13}>
-                                        <LivroMelhores {...livro} />
-                                    </div>
-                                })
-                            })
-                            : <div className="divLoading">
-                                <span className="loading">Carregando a lista de livros...</span>
-                            </div>
-                        }
+  render() {
+    return (
+      <Fragment>
+        <Header
+          titulo="Os melhores livros da semana"
+          classe="melhores-header"
+        />
+        <section className="container melhores">
+          <Intro
+            imagem={NYT}
+            alt="Capa do jornal impresso The New York Times"
+            paragrafo1={[
+              "O jornal ",
+              <span key="indicadorMelhores" className="nomeSite">
+                The New York Times
+              </span>,
+              " é uma publicação norte-americana que circula diariamente desde 1851. Considerada uma referência no mundo do jornalismo, a publicação traz semanalmente uma lista dos livros que mais foram vendidos nos Estados Unidos.",
+            ]}
+            paragrafo2='Esses "best-sellers" são originalmente agrupados em diversas categorias e, aqui, você tem acesso à lista completa: confira quais foram os 90 livros mais populares na última semana!'
+          />
+          <div>
+            {this.state.livros.length > 0 ? (
+              this.state.livros.map((item) => {
+                return item.flatMap((livro) => {
+                  return (
+                    <div key={livro.primary_isbn13}>
+                      <LivroMelhores {...livro} />
                     </div>
-                </section>
-            </Fragment>
-        )
-    }
+                  );
+                });
+              })
+            ) : (
+              <div className="divLoading">
+                <span className="loading">Carregando a lista de livros...</span>
+              </div>
+            )}
+          </div>
+        </section>
+      </Fragment>
+    );
+  }
 }
 
 export default Melhores;
