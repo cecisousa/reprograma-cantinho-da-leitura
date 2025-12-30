4c579921fc60fca48e119fa25cda14b16095d842
cecisousa <ceciliasousa21@gmail.com>
2025-12-30 13:11:39 +0100

    Refactor MelhoresLivros page

--- cantinhodaleitura/app/melhores-da-semana/components/LivroMelhores/index.tsx
+++ cantinhodaleitura/app/melhores-da-semana/components/LivroMelhores/index.tsx
@@ -1,37 +1,46 @@
-import React from 'react';
-import './styles.css';
+import Image from "next/image";
+import "./styles.css";
 
 interface LivroMelhoresProps {
-  title: string;
-  author: string;
-  description: string;
-  book_image: string;
+	title: string;
+	author: string;
+	description: string;
+	book_image: string;
 }
 
-const LivroMelhores: React.FC<LivroMelhoresProps> = (props) => {
-  let descricao = props.description;
-  if (descricao === '') {
-    descricao =
-      'Conteúdo não disponibilizado pelo jornal The New York Times.';
-  }
+const LivroMelhores = (props: LivroMelhoresProps) => {
+	let descricao = props.description;
+	if (descricao === "") {
+		descricao = "Conteúdo não disponibilizado pelo jornal The New York Times.";
+	}
 
-  return (
-    <div className="melhoresContent">
-      <div className="divMelhoresImagem">
-        <img
-          className="melhoresImagem"
-          src={props.book_image}
-          alt="Lista de livros do NYTimes"
-        />
-      </div>
-      <div className="melhoresTexto">
-        <h3 className="melhoresTitulo">{props.title}</h3>
-        <p className="melhoresAutor">{props.author}</p>
-        <p className="melhoresDescricao">{descricao}</p>
-      </div>
-    </div>
-  );
+	// Truncate description if too long
+	const maxLength = 250;
+	const truncatedDesc =
+		descricao.length > maxLength
+			? `${descricao.substring(0, maxLength)}...`
+			: descricao;
+
+	return (
+		<div className="livro-card">
+			<div className="livro-image-wrapper">
+				<Image
+					className="livro-image"
+					src={props.book_image}
+					alt={`Capa do livro ${props.title}`}
+					width={180}
+					height={270}
+				/>
+			</div>
+			<div className="livro-content">
+				<div className="livro-header">
+					<h3 className="livro-title">{props.title}</h3>
+					<p className="livro-author">por {props.author}</p>
+				</div>
+				<p className="livro-description">{truncatedDesc}</p>
+			</div>
+		</div>
+	);
 };
 
 export default LivroMelhores;
-
