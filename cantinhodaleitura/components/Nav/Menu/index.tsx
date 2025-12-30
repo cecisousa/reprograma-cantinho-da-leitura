9bf32816b7a8c91cbdbe11a0d92996b7a3786795
cecisousa <ceciliasousa21@gmail.com>
2025-12-30 15:15:40 +0100

    Refactor Menu component to improve interaction

--- cantinhodaleitura/components/Nav/Menu/index.tsx
+++ cantinhodaleitura/components/Nav/Menu/index.tsx
@@ -1,59 +1,95 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import Link from 'next/link';
-import './styles.css';
+import { useState } from "react";
+import type { KeyboardEvent } from "react";
+import Link from "next/link";
+import "./styles.css";
 
 const Menu: React.FC = () => {
-  const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-  const handleOpenClose = () => {
-    setOpen((prevState) => !prevState);
-  };
+	const handleOpenClose = () => {
+		setOpen((prevState) => !prevState);
+	};
 
-  let classeDasOpcoes = 'navbar-menu__opcoes';
-  let classeDoBotao = 'navbar-menu__botao';
+	const handleKeyDown = (e: KeyboardEvent<HTMLButtonElement>) => {
+		if (e.key === "Enter" || e.key === " ") {
+			e.preventDefault();
+			handleOpenClose();
+		}
+	};
 
-  if (open) {
-    classeDasOpcoes += ' navbar-menu__opcoes--aberto';
-    classeDoBotao += ' navbar-menu__botao--aberto';
-  }
+	const handleLinkClick = () => {
+		setOpen(false);
+	};
 
-  return (
-    <div className="nav-menu navbar-links">
-      <span className={classeDoBotao} onClick={handleOpenClose}>
-        Menu
-      </span>
-      <ul className={classeDasOpcoes}>
-        <li>
-          <Link href="/" className="navbar-links__ativo">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/por-que-ler" className="navbar-links__ativo">
-            Por que ler?
-          </Link>
-        </li>
-        <li>
-          <Link href="/melhores-da-semana" className="navbar-links__ativo">
-            Os melhores
-          </Link>
-        </li>
-        <li>
-          <Link href="/busca-autores" className="navbar-links__ativo">
-            Autores
-          </Link>
-        </li>
-        <li>
-          <Link href="/dica-especial" className="navbar-links__ativo">
-            Dica especial
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
+	let classeDasOpcoes = "navbar-menu__opcoes";
+	let classeDoBotao = "navbar-menu__botao";
+
+	if (open) {
+		classeDasOpcoes += " navbar-menu__opcoes--aberto";
+		classeDoBotao += " navbar-menu__botao--aberto";
+	}
+
+	return (
+		<div className="nav-menu navbar-links">
+			<button
+				className={classeDoBotao}
+				onClick={handleOpenClose}
+				onKeyDown={handleKeyDown}
+				type="button"
+			>
+				Menu
+			</button>
+			<ul className={classeDasOpcoes}>
+				<li>
+					<Link
+						href="/"
+						className="navbar-links__ativo"
+						onClick={handleLinkClick}
+					>
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link
+						href="/por-que-ler"
+						className="navbar-links__ativo"
+						onClick={handleLinkClick}
+					>
+						Por que ler?
+					</Link>
+				</li>
+				<li>
+					<Link
+						href="/melhores-da-semana"
+						className="navbar-links__ativo"
+						onClick={handleLinkClick}
+					>
+						Os melhores
+					</Link>
+				</li>
+				<li>
+					<Link
+						href="/busca-autores"
+						className="navbar-links__ativo"
+						onClick={handleLinkClick}
+					>
+						Autores
+					</Link>
+				</li>
+				<li>
+					<Link
+						href="/dica-especial"
+						className="navbar-links__ativo"
+						onClick={handleLinkClick}
+					>
+						Dica especial
+					</Link>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Menu;
-
